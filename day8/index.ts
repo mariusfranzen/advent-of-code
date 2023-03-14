fcd21de1f0128709f776a47bdbc13e687e9503cb
mariusfranzen <marius.franzen1@gmail.com>
2023-03-14 09:59:05 +0100

    2d array

--- day8/index.ts
+++ day8/index.ts
@@ -1,3 +1,19 @@
 #!/usr/bin/env node
+import fs = require('fs');
 
-console.log('Hello World');
+const text: string = fs.readFileSync('./input.txt').toString();
+const lines: string[] = text.split('\n');
+
+const input: number[][] = [];
+
+for (let y = 0; y < lines.length; y++) {
+    const newRow: number[] = [];
+    for (let x = 0; x < lines[y].length; x++) {
+        if (!Number.isNaN(Number.parseInt(lines[y][x]))) {
+            newRow.push(Number.parseInt(lines[y][x]));
+        }
+    }
+    input.push(newRow);
+}
+
+
