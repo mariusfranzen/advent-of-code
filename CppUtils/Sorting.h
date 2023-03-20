6aecfc3b049474c655f7667f9be434873abb8b2f
mariusfranzen <marius.franzen1@gmail.com>
2023-03-20 10:44:32 +0100

    documentation

--- CppUtils/Sorting.h
+++ CppUtils/Sorting.h
@@ -5,7 +5,18 @@ namespace CppUtils
 	class Sorting
 	{
 	public:
+		/**
+		 * \brief Sorts an int array with QuickSort
+		 * \param arr the array to sort
+		 * \param start starting index for sorting
+		 * \param end end index for sorting
+		 */
 		static void QuickSort(int* arr, int start, int end);
+		/**
+		 * \brief Inverses an array
+		 * \param arr The array to reverse
+		 * \param len Length of array
+		 */
 		static void Reverse(int* arr, int len);
 	private:
 		static int Partition(int* arr, int start, int end);
--- CppUtils/StringUtils.h
+++ CppUtils/StringUtils.h
@@ -6,6 +6,11 @@ namespace CppUtils
 	class StringUtils
 	{
 	public:
+		/**
+		 * \brief Checks if a string consists of only numbers
+		 * \param s String to check
+		 * \return true if string is number
+		 */
 		static bool IsNumber(const std::string& s);
 	};
 }
